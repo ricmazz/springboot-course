c0699e7e93dbd7935683c94fb48bca84a3eecb19
Ricardo Mazzarioli <ricardomazzarioli@outlook.com>
2026-01-28 01:44:35 -0300

    User entity and resource

---
+++ src/main/java/com/arasys/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.arasys.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.arasys.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "ricardo", "ricardomazzarioli@outlook.com", "18996580654", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
